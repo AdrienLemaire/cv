58ca542ac3c993c94d3d795b30fb576dab84a424
Adrien Lemaire <lemaire.adrien@gmail.com>
2022-06-17 17:07:55 +0200

    feat: init CV layout

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      inset: {
+        '1/5': '20%',
+      }
+    },
+  },
+  plugins: [],
+}
